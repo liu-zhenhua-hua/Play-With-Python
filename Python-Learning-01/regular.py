788a91c0994da9099b3e65bfc231e7325fc49b59
Liu Zhenhua <join_github_repo@126.com>
2020-08-19 19:25:06 +0800

    Adding a Python File for Regular Expression

---
+++ Python-Learning-01/regular.py
@@ -0,0 +1,38 @@
+'''
+ Python Regular Package;
+'''
+import re;
+
+text_to_search='''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+
+#print(r'\tTab');
+
+pattern = re.compile(r'abc');
+matches = pattern.finditer(text_to_search);
+
+for match in matches:
+    print(match)
+
+print(text_to_search[1:4])
