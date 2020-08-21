7209f4958f49ea813e2ea71d5615afa83c91bae7
Liu Zhenhua <join_github_repo@126.com>
2020-08-21 22:18:17 +0800

    Updating new Regular Expression File

--- Python-Learning-01/myRegular.py
+++ Python-Learning-01/myRegular.py
@@ -8,8 +8,11 @@ regular_text='''
 '''
 
 pattern = re.compile(r'\d{3}\.\d{3}\.\d{4}')
+pattern = re.compile(r'\d{3}\-\d{3}\-\d{4}')
+
 
 matches = pattern.finditer(regular_text)
 
 for match in matches:
     print(match)
+
