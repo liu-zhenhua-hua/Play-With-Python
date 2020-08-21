ff682f5fd45a6fd8753de96ccbe07d02ea962ff5
Liu Zhenhua <join_github_repo@126.com>
2020-08-21 22:16:28 +0800

    Updating new Regular Expression File

--- Python-Learning-01/myRegular.py
+++ Python-Learning-01/myRegular.py
@@ -6,3 +6,10 @@ regular_text='''
 135-900-8745
 190-211-1098
 '''
+
+pattern = re.compile(r'\d{3}\.\d{3}\.\d{4}')
+
+matches = pattern.finditer(regular_text);
+
+for match in matches:
+    print(match)
