607bc5b5ae6c509e745bd8f615385d484acf801e
Liu Zhenhua <join_github_repo@126.com>
2020-09-03 19:43:10 +0800

    Creating new ListFirst.py

---
+++ Python-Learning-01/ListFirst.py
@@ -0,0 +1,24 @@
+'''
+Python List Program
+'''
+
+
+# my_list = ["first","second","third","fourth"]
+#
+# print(my_list[1:-1])
+# print(my_list[1:3])
+
+"""
+Python Slice Operation
+"""
+
+my_second_list = ["Python","openCV","PyQT","CERT","Java"]
+
+print("Python Slice Operation ")
+
+
+print(my_second_list[:]) #list all the content
+print(my_second_list[:3]) # ["Python","openCV","PyQT"]
+print(my_second_list[1:])
+
+
