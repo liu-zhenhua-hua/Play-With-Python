398d21e3ca33dcfe84cf0933cd76771ffd438626
Liu Zhenhua <join_github_repo@126.com>
2020-09-03 16:41:08 +0800

    Updating the StringFirst Python File

---
+++ Python-Learning-01/StringFirst.py
@@ -0,0 +1,44 @@
+'''
+
+ "Hello Python, but we can't this is message in the output of the program"
+ ' both single and double quote can be used at same time'
+
+'''
+
+
+aMessage = """ Here is another Message in the Tom's Bag  """
+
+print(aMessage)
+
+"""
+Python's number
+"""
+
+aNumber = -2E-8
+
+print(aNumber)
+
+del aNumber
+
+"""
+获取用户输入
+
+print("=============== User Message ===============")
+name = input("Your Name ? : ")
+
+print("Hello " + name)
+
+age = input("Your Age ? : ")
+
+print("Your Age : " + age)
+
+
+print("=====================Python's List Example ======================")
+"""
+my_list = []
+ty_list = []
+
+length = len(my_list)
+
+print("my_list's length " + str(length))
+
