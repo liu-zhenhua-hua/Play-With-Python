5ac22f28c306a7cb06521ac1a30e05d65bdb7423
Liu Zhenhua <join_github_repo@126.com>
2020-09-03 20:25:06 +0800

    Updating ListFirst.py

--- Python-Learning-01/ListFirst.py
+++ Python-Learning-01/ListFirst.py
@@ -8,10 +8,12 @@ Python List Program
 # print(my_list[1:-1])
 # print(my_list[1:3])
 
+
 """
 Python Slice Operation
 """
 
+""" 
 my_second_list = ["Python","openCV","PyQT","CERT","Java"]
 
 print("Python Slice Operation ")
@@ -20,5 +22,26 @@ print("Python Slice Operation ")
 print(my_second_list[:]) #list all the content
 print(my_second_list[:3]) # ["Python","openCV","PyQT"]
 print(my_second_list[1:])
+
+
+my_second_list[len(my_second_list):] = ["Linux","GNU","JavaScript"] #在List尾追加列表
+
+print(my_second_list[:])
+
+
+my_second_list[:0] = ["CentOS"] #在表头添加列表
+
+print(my_second_list[:])
+
+my_second_list[:0] = [] #在表头添加一个空列表
+
+print(my_second_list)
+
+
+"""
+
+"""
+ Python List insert, del remove operation
+"""
 
 
