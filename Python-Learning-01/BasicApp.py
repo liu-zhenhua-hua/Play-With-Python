4956c0d0cd4daa893b29adace16b522190263834
Liu Zhenhua <join_github_repo@126.com>
2020-09-03 14:35:29 +0800

    Updating File

--- Python-Learning-01/BasicApp.py
+++ Python-Learning-01/BasicApp.py
@@ -1,8 +1,6 @@
 '''
  Python Foundation program
 '''
-
-
 a = "a Python Message"
 print(a)
 
@@ -19,6 +17,8 @@ print("Multiple Line comment")
 
 print("===================================================================")
 
+""" 
+    注释掉循环
 n = 1
 
 while n < 9:
@@ -31,3 +31,12 @@ n = 15
 while n > 0:
     print(n)
     n = n - 1
+
+"""
+
+a = [1,2,3]
+b = a
+c = b
+b[1] = 5
+print(a,b,c)
+
