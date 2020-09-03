05624db030e4bed2d3fa87c94cb30bc102404fb0
Liu Zhenhua <join_github_repo@126.com>
2020-09-03 14:59:31 +0800

    Updating BasicApp Again Python File

---
+++ Python-Learning-01/BasicAppAgain.py
@@ -0,0 +1,30 @@
+'''
+This is Basic Python Programming File Again,
+The First File is BasicApp
+'''
+
+x = "Hello Python Programming Language"
+print(x)
+
+x = 5
+print(x)
+
+#del x
+#print(x) #after delete pointer x, print it again, we will get the error message
+
+x1 = 5
+x2 = 12
+result = (x1 + x2) / 2
+
+print(result) #Python 3 中整数的除法, 将保留小数点儿, 如果要截断小数部分, 则使用 //
+
+y1 = 15
+y2 = 3
+
+y_result = (y1 + y2) //2
+print(y_result)
+
+print("=====================")
+
+y_result = (y1 + y2) /2
+print(y_result)
