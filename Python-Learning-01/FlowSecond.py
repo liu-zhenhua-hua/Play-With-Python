605d385ef83fb5ed748b6c9d2d249cf9a67753de
Liu Zhenhua <join_github_repo@126.com>
2020-09-04 17:28:11 +0800

    Updating FlowSecond Python File

---
+++ Python-Learning-01/FlowSecond.py
@@ -0,0 +1,34 @@
+'''
+Python Flow Application
+'''
+
+alist = [(3,4),(6,8),(10,1)]
+result = 0
+for x,y in alist:
+    result = result + (x * y)
+
+print(result)
+
+
+'''
+enumerate
+'''
+
+blist = [1,3,-7,4,9,-5,4]
+result = enumerate(blist)
+
+print(result)
+
+clist = [2,4,6,8]
+dlist = [1,3,5,7]
+
+print("================================================================")
+eresult = zip(clist,dlist)
+new_collection = tuple(eresult)
+print(new_collection)
+cresult = 0
+for a,b in new_collection:
+    cresult = cresult + (a + b)
+
+print(cresult)
+
