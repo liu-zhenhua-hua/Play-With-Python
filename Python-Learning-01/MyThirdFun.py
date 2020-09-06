1fdeb246650e50c63e3da52aa1e9812f2a7c89e1
Liu Zhenhua <join_github_repo@126.com>
2020-09-06 10:40:52 +0800

    Updating the MyThirdFun Python File

---
+++ Python-Learning-01/MyThirdFun.py
@@ -0,0 +1,22 @@
+'''
+Pyhon Function
+'''
+
+"""
+mutable object for function argument
+"""
+
+def my_fun(n,list1,list2):
+    list1.append(3)
+    list2 = [4,5,6]
+    n += 1
+
+x = 5
+y = [1,2]
+z = [4,5]
+
+my_fun(x,y,z)
+
+print(x)
+print(y)
+print(z)
