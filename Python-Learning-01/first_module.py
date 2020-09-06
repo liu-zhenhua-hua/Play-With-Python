af96e29d8910380ec803d54a8815f8ad3569af59
Liu Zhenhua <join_github_repo@126.com>
2020-09-06 17:45:21 +0800

    Updating first_module Python File

--- Python-Learning-01/first_module.py
+++ Python-Learning-01/first_module.py
@@ -6,6 +6,9 @@ print(__name__)
 def first_function():
     print("The First Module Function ! ")
 
+def second_function():
+    print("The Second Module Function ! ")
 
 if __name__ == "__main__":
     first_function()
+    second_function()
