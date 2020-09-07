b0adef02d5af5c7e5e7e2297ae64317537d86aa3
Liu Zhenhua <join_github_repo@126.com>
2020-09-07 09:57:18 +0800

    Updating pyargs_first Python File

--- Python-Learning-01/pyargs_first.py
+++ Python-Learning-01/pyargs_first.py
@@ -10,6 +10,7 @@ def main_method():
     print(len(sys.argv))
     if(len(sys.argv) == 1):
         print("Missing arguments for this program ! ")
-
+    else:
+        print("Processing the detail logic ")
 if __name__ == "__main__":
     main_method()
