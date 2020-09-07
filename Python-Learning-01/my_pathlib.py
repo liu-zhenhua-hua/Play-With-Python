b1167929a449902fe02d947e685e6a4a113ad391
Liu Zhenhua <join_github_repo@126.com>
2020-09-07 19:59:57 +0800

    Updating the my_pathlib Python File

--- Python-Learning-01/my_pathlib.py
+++ Python-Learning-01/my_pathlib.py
@@ -9,3 +9,13 @@ pathlib_textfile.write_text("This is text",encoding="UTF-8")
 
 pathlib_textfile2 = Path("pathlib_file2")
 pathlib_textfile2.write_text("The Second Path lib File",encoding="UTF-8")
+
+
+
+"""
+Writing binary data into a file by Path Object
+"""
+pathlib_binaryfile = Path("my_binary_file")
+pathlib_binaryfile.write_bytes(b'Binary File Contents')
+
+print(pathlib_binaryfile.read_bytes())
