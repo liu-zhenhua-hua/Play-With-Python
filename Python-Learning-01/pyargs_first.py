7f1c88e90e4f94054675d6c3f225d66f631dd97d
Liu Zhenhua <join_github_repo@126.com>
2020-09-07 09:48:48 +0800

    Adding pyargs_first Python File

---
+++ Python-Learning-01/pyargs_first.py
@@ -0,0 +1,13 @@
+#! /Users/tony/anaconda3/bin/python3
+'''
+Python process argument
+'''
+import sys
+import pandas
+def main_method():
+    print("Anaconda Python Platform")
+    print(sys.argv)
+
+
+if __name__ == "__main__":
+    main_method()
