a5840c453171c997c7ac0618872c449620773cce
Liu Zhenhua <join_github_repo@126.com>
2020-09-08 14:20:37 +0800

    Updating Second Exception Python File

--- Python-Learning-01/my_second_exception.py
+++ Python-Learning-01/my_second_exception.py
@@ -7,11 +7,11 @@ def process_alist(alist,index):
         print(indexError)
     except ArithmeticError as arithmeticError:
         print(arithmeticError)
-    except:
+    except: #这是try语句块最后一个except子句,它可以不指定任何异常类型, 这样就可以处理任何异常类型
         print("our process_alist method got error ")
-    else: #如果try没有捕获到异常, 就会来到else子句当中
+    else: #如果try没有捕获到异常, 就会来到else子句当中  ----> else是可选的
         print("We are in the Exception 'ELSE' Statement Block ")
-    finally: #finally 子句的部分是无论如何都会被执行
+    finally: #finally 子句的部分是无论如何都会被执行 ------> finally子句也是可选的, finally 这里通常可以进行, 资源清理操作, 例如关闭数据库, 文件
         print("The Exception Block has been executed and Exit noramlly ")
 
 
