13cfacd99474e0ec540a33803601f9107656f599
Liu Zhenhua <join_github_repo@126.com>
2020-09-08 13:52:56 +0800

    Adding new Exception Python File

--- Python-Learning-01/my_Exception.py
+++ Python-Learning-01/my_Exception.py
@@ -8,4 +8,10 @@ def process_alist(arg_alist,index):
     try:
         arg_alist[index]
     except IndexError:
-        print("--- List index out of range")
+        print("Your List Index out of Range  ^_^")
+        # raise IndexError("Your List Index out of Range  ^_^") #这是个很有意思的异常处理, 试试这个语句, 看看运行结果
+
+
+alist = [5,9,10]
+
+process_alist(alist,6)
