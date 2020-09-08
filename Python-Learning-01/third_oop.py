800c21e0e43a3fc68f040569185d32cb39bf0844
Liu Zhenhua <join_github_repo@126.com>
2020-09-08 20:53:04 +0800

    Updating the third Python File

--- Python-Learning-01/third_oop.py
+++ Python-Learning-01/third_oop.py
@@ -1,17 +1,17 @@
 #!/Users/tony/anaconda3/bin/python3
 
-"""
-define a class
-"""
-
 class Employee:
-    def __int__(self,firstName,lastName,salary):
-        self.firstName = firstName
-        self.lastName = lastName
+    def __init__(self,first,last,salary): #pay a attention __init__
+        self.first = first
+        self.last = last
         self.salary = salary
 
     def fullname(self):
-        return '{},{}'.format(self.firstName,self.lastName)
+        return '{},{}'.format(self.first,self.last)
 
 
-empA = Employee()
+# empA = Employee('Tony','Liu',500)
+
+empA = Employee('Tony','Liu',900)
+print(empA.fullname())
+print(Employee.fullname(empA))
