bdf267c4861491f8219c4b1278d3e78f34ad4d28
Liu Zhenhua <join_github_repo@126.com>
2020-09-08 16:29:31 +0800

    Updating the first_oop Python File

---
+++ Python-Learning-01/first_oop.py
@@ -0,0 +1,15 @@
+#! /Users/tony/anaconda3/bin/python3
+
+class Employee:
+    pass
+
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+print(emp_1)
+print(emp_2)
+
+emp_1.first = 'Tony'
+emp_1.last = 'Liu'
+emp_1.email='tl_mail@gmail.com'
