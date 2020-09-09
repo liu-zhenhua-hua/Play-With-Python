72fa95708046a1cfb7524315c7a413d1fabc4ba2
Liu Zhenhua <join_github_repo@126.com>
2020-09-09 09:49:38 +0800

    Adding new variable in oop Python File

--- Python-Learning-01/class_var.py
+++ Python-Learning-01/class_var.py
@@ -1,1 +1,14 @@
 #!/Users/tony/anaconda3/bin/python3
+class Student:
+    def __init__(self, first, last, score, email):
+        self.first = first
+        self.last = last
+        self.score = score
+        self.email = email
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+student_1 = Student('Tony', 'Liu', 90, 'liu@gmail.com')
+student_2 = Student('Mark', 'Li', 89, 'mark@gmail.com')
---
+++ Python-Learning-01/variables_in_oop.py
@@ -0,0 +1,22 @@
+#!/Users/tony/anaconda3/bin/python3
+class Employee:
+    raise_amount = 1.04
+
+    def __init__(self, firstname, lastname, salary):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.salary = salary
+
+    def fullname(self):
+        return '{} {}'.format(self.firstname, self.lastname)
+
+    def pay_raise(self):
+        return self.salary * self.raise_amount
+
+
+emp_1 = Employee('Tony', 'Liu', 50000)
+emp_2 = Employee('Mark', 'Zhang', 60000)
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
