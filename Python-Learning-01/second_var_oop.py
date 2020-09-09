46d2a02a8bc639c44d7c2182b39670547d6a15a3
Liu Zhenhua <join_github_repo@126.com>
2020-09-09 11:15:51 +0800

    Updating second variable Python File

--- Python-Learning-01/second_var_oop.py
+++ Python-Learning-01/second_var_oop.py
@@ -20,13 +20,29 @@ class Employee:
     def set_raise_amt(cls, amount):
         cls.raise_amount = amount
 
+    """
+    也可以使用classmethod定义一个类的构造函数, 传统Python构造器的另一个选择
+    """
+    @classmethod
+    def from_string_object(cls, object_str):
+        firstname, lastname, salary = object_str.split('-')
+        return cls(firstname, lastname, salary)
+
+print("=======================================================================================")
 
 emp_1 = Employee('Tony', 'Liu', 50000)
 emp_2 = Employee('Mark', 'Jiang', 60000)
 emp_3 = Employee('James', 'Li', 80000)
 
-Employee.set_raise_amt(1.07)
+# Employee.set_raise_amt(1.07)
+#
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+# print(emp_3.raise_amount)
 
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
-print(emp_3.raise_amount)
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Job-80000'
+emp_str_3 = 'Jane-Doe-90000'
+
+new_emp_1 = Employee.from_string_object(emp_str_1)
+new_emp_2 = Employee.from_string_object(emp_str_2)
