8c2e379c24f009c2a64e348305c083e4618c7558
Liu Zhenhua <join_github_repo@126.com>
2020-09-10 17:48:12 +0800

    Updating the fourth Python File

--- Python-Learning-01/oop/fourth_oop.py
+++ Python-Learning-01/oop/fourth_oop.py
@@ -16,11 +16,28 @@ class Employee:
         return '{} {}'.format(self.firstname, self.lastname)
 
     def pay_raise(self):
-        return self.salary * Employee.raise_amount
+        return self.salary * self.raise_amount
 
 
 emp_1 = Employee('Tony', 'Liu', 50000)
 emp_2 = Employee('Mark', 'Zhang', 60000)
+emp_3 = Employee('Jane', 'Doe', 70000)
 
-print(Employee.object_number)
+# print(Employee.object_number)
 
+print("========================= Employee.raise_amount = 1.04 ========================")
+
+print(emp_1.pay_raise())
+print(emp_2.pay_raise())
+print(emp_3.pay_raise())
+
+Employee.raise_amount = 1.05
+
+print("========================= Employee.raise_amount = 1.05 ========================")
+
+print(emp_1.pay_raise())
+print(emp_2.pay_raise())
+print(emp_3.pay_raise())
+
+
+print("========================= Employee.raise_amount = 1.05 ========================")
