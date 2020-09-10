365de015ac15448478c100115a46928f82d3ddaa
Liu Zhenhua <join_github_repo@126.com>
2020-09-10 08:32:39 +0800

    Updating the Python File

--- Python-Learning-01/oop/first_oop.py
+++ Python-Learning-01/oop/first_oop.py
@@ -9,3 +9,16 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.firstname, self.lastname)
 
+    def email_addr(self):
+        return '{}.{}@gmail.com'.format(self.firstname, self.lastname)
+
+    def salar_info(self):
+        return self.salary
+
+
+
+emp_1 = Employee('Tony', 'Liu', 50000)
+emp_2 = Employee('Mark', 'Jiang', 60000)
+
+print(emp_1)
+print(emp_2)
