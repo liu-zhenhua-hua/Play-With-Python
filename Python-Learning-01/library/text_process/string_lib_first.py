52f342803a8c144c400b6eb4569ad848a0d83bae
Liu Zhenhua <join_github_repo@126.com>
2020-09-14 13:29:25 +0800

    Updating the Python File

---
+++ Python-Learning-01/library/text_process/string_lib_first.py
@@ -0,0 +1,14 @@
+#!/Users/tony/anaconda3/bin/python3
+
+"""
+Python string.py standard library.
+"""
+
+import string
+
+"""
+string class's constant (PEP 3101 - Advanced String Formatting)
+PEP Python Enhancement Proposal
+"""
+print(string.ascii_lowercase)
+print(string.printable)
