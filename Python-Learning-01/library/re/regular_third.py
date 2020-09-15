24dde90777c9de5c5cfa392a95b51cffd620a176
Liu Zhenhua <join_github_repo@126.com>
2020-09-15 19:36:18 +0800

    Updating the Python File

---
+++ Python-Learning-01/library/re/regular_third.py
@@ -0,0 +1,15 @@
+#!/Users/tony/anaconda3/bin/python3
+
+import re
+
+"""
+\w  word characters, 这里的word characters 指的是这些(a-z, A-Z,0-9,_) 
+\W  大写的W, 通常是小写w的反义, 
+"""
+
+my_search_text = """[]AW"""
+
+pattern = re.compile(r'\W')
+matches = pattern.finditer(my_search_text)
+for items in matches:
+    print(items)
