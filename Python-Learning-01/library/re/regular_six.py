63ce7eb212afb032c2c28a9c89c2eb916d45ecee
Liu Zhenhua <join_github_repo@126.com>
2020-09-27 09:18:57 +0800

    Updating the Python File

--- Python-Learning-01/library/re/regular_six.py
+++ Python-Learning-01/library/re/regular_six.py
@@ -1,5 +1,15 @@
 #!/Users/tony/anaconda3/bin/python3
 
-"""
+import re
+
+our_phone_numbers = """
+890-145-8946 
+326-226-6433
 
 """
+
+pattern = re.compile(r'\d\d\d')
+matches = pattern.finditer(our_phone_numbers)
+
+for items in matches:
+    print(items)
