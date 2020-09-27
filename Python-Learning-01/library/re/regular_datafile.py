4dc649ae8ed5e01c38788827486fed76df1e5e0c
Liu Zhenhua <join_github_repo@126.com>
2020-09-27 17:03:14 +0800

    Updating the Python File

---
+++ Python-Learning-01/library/re/regular_datafile.py
@@ -0,0 +1,13 @@
+#!/Users/tony/anaconda3/bin/python3
+
+import re
+
+pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
+
+with open('data.txt','r',encoding='utf-8') as f:
+    contents = f.read()
+
+    matches = pattern.finditer(contents)
+
+    for items in matches:
+        print(items)
