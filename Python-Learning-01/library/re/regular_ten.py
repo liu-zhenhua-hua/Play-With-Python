ee987d3c03a91f599675f4ff0cc26c3f1f59fa71
Liu Zhenhua <join_github_repo@126.com>
2020-09-28 16:35:35 +0800

    Updating the Python File

--- Python-Learning-01/library/re/regular_ten.py
+++ Python-Learning-01/library/re/regular_ten.py
@@ -16,3 +16,20 @@ pattern = re.compile(r'[a-zA-z]+@[a-zA-Z]+\.com')
 matches = pattern.finditer(my_email_list)
 for items in matches:
     print(items)
+
+print("=" * 90)
+"""
+下面我们再将其余的email地址取出
+"""
+
+pattern = re.compile(r'[a-zA-Z0-9-.]+@[a-zA-Z-]+\.(com|edu|net)')
+matches = pattern.finditer(my_email_list)
+for items in matches:
+    print(items)
+
+print("=" * 90)
+
+pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+')
+matches = pattern.finditer(my_email_list)
+for items in matches:
+    print(items)
