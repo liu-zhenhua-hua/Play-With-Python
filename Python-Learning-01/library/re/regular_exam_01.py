389fda9e009bca99e5b02519eb8c9ee2de257b66
Liu Zhenhua <join_github_repo@126.com>
2020-09-28 20:02:17 +0800

    Updating the Python File

--- Python-Learning-01/library/re/regular_exam_01.py
+++ Python-Learning-01/library/re/regular_exam_01.py
@@ -7,6 +7,17 @@ Python Regular Expression Example-01
 """
 
 original_text = """
-
-
+899-897-9098
+457-456-1256
+887=MLK=9807
 """
+
+pattern = re.compile(r'\d{3}=\w+=\d{4}') # 匹配887=MLK=9807
+matches = pattern.finditer(original_text)
+
+for items in matches:
+    print(items)
+
+
+print("=" * 90)
+
