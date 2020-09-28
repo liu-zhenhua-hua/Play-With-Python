b5e4ba0e18dec1a47e2635205a24ff379057e7c0
Liu Zhenhua <join_github_repo@126.com>
2020-09-28 16:56:01 +0800

    Updating the Python File

--- Python-Learning-01/library/re/regular_ten.py
+++ Python-Learning-01/library/re/regular_ten.py
@@ -33,3 +33,35 @@ pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
 matches = pattern.finditer(my_email_list)
 for items in matches:
     print(items)
+
+print("=" * 90)
+
+my_urls = """
+https://www.google.com
+http://coreyms.com
+https://youtube.com
+https://www.nasa.gov
+"""
+
+pattern = re.compile(r'https?://(www\.)?\w+\.\w+')
+matches = pattern.finditer(my_urls)
+for items in matches:
+    print(items)
+
+print("=" * 90 )
+"""
+我们还可以这样写
+"""
+pattern = re.compile(r'https?://(www\.)?(\w+)(\.\w+)')
+matches = pattern.finditer(my_urls)
+for items in matches:
+    print(items.group(0)) #这里用到了group()方法
+
+print("=" * 90)
+"""
+urls 如果我们取出domain.com的信息, 可以这样写
+"""
+pattern = re.compile(r'https?://(www\.)?(\w+)(\.\w+)')
+
+subbed_urls = pattern.sub(r'\2\3',my_urls) #这里用了sub方法
+print(subbed_urls)
