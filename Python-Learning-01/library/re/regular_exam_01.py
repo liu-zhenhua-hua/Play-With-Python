06f5fcf4a4e0f2b720ac1ab3797c9e27c4c0d197
Liu Zhenhua <join_github_repo@126.com>
2020-09-28 20:28:51 +0800

    Updating the Python File

--- Python-Learning-01/library/re/regular_exam_01.py
+++ Python-Learning-01/library/re/regular_exam_01.py
@@ -21,3 +21,33 @@ for items in matches:
 
 print("=" * 90)
 
+original_numbers = """
+899-897-9098
+457-456-1256
+887=MLK=9807
+670#BNM#7856
+809^BHU^8900
+"""
+
+"""
+从 original_numbers 中匹配下面这些值
+887=MLK=9807
+670#BNM#7856
+809^BHU^8900
+"""
+pattern = re.compile(r'\d{3}(=|#|\^)\w+(=|#|\^)\d{4}')
+matches = pattern.finditer(original_numbers)
+for items in matches:
+    print(items)
+
+
+print("=" * 90)
+
+
+original_name_list = """
+Ms. Schafer
+Mr. Bourne
+Ms. Taras
+Mrs. T 
+"""
+
