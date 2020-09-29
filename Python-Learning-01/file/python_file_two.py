98764bdbd1097940e8ea3d959377714d574b107c
Liu Zhenhua <join_github_repo@126.com>
2020-09-29 14:00:39 +0800

    Updating the Python File

--- Python-Learning-01/file/python_file_two.py
+++ Python-Learning-01/file/python_file_two.py
@@ -1,2 +1,11 @@
 #!/Users/tony/anaconda3/bin/python3
 
+"""
+using Python context manager
+"""
+"""
+使用下面的代码输出文件的全部内容
+"""
+with open('my_file.txt','r') as file:
+    for line in file:
+        print(line,end='')
