7a0f96f497365b31be923b6d9eadd1306135a61a
Liu Zhenhua <join_github_repo@126.com>
2020-09-29 13:51:52 +0800

    Updating the Python File ;

--- Python-Learning-01/file/python_file_one.py
+++ Python-Learning-01/file/python_file_one.py
@@ -10,3 +10,24 @@ print(f.name)
 print(f.mode)
 
 f.close() #close the file release the source.
+print("="*90)
+"""
+using Python context manager
+使用Python context manager
+with context manager 可以打开文件
+使用完文件,同时也关闭了文件, 都是由context manager来进行管理
+"""
+
+with open('my_file.txt','r') as file:
+    f_contents = file.read()
+    print(f_contents)
+
+print("="*90)
+"""
+看看下面这段代码
+"""
+with open('my_file.txt','r') as file:
+    pass
+
+print(file.closed)
+print(f.read())
