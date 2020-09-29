32368bca889ca78741d60eb6a07ffd6d798b24d1
Liu Zhenhua <join_github_repo@126.com>
2020-09-29 14:52:44 +0800

    Updating the Python File

--- Python-Learning-01/file/python_file_two.py
+++ Python-Learning-01/file/python_file_two.py
@@ -9,3 +9,18 @@ using Python context manager
 with open('my_file.txt','r') as file:
     for line in file:
         print(line,end='')
+
+
+
+"""
+读取文件时, 还可以指定读取内容的大小,
+下面这段代码, 同样可以遍历文件中的内容
+"""
+
+with open('my_file.txt','r') as file:
+    size_to_read = 100
+    f_content = file.read(size_to_read)
+
+    while len(f_content) > 0:
+        print(f_content,end='')
+        f_content = file.read(size_to_read)
