3f7f4dfa040d5bb9b4f820c7d4a7a5ef249f465b
Liu Zhenhua <join_github_repo@126.com>
2020-09-29 13:19:51 +0800

    Updating the Python File

--- Python-Learning-01/file/python_file_one.py
+++ Python-Learning-01/file/python_file_one.py
@@ -1,2 +1,11 @@
 #!/Users/tony/anaconda3/bin/python3
 
+"""
+Python File Object
+"""
+
+f = open('my_file.txt','r') #built-in function open()
+
+print(f.name)
+
+f.close() #close the file release the source.
