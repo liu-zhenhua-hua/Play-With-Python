a720d7cb63778367167415c736943ed808d02dfa
Liu Zhenhua <join_github_repo@126.com>
2020-10-02 00:11:00 +0800

    Updating the Python File

--- Python-Learning-01/lang/flow/python_flow-loop_one.py
+++ Python-Learning-01/lang/flow/python_flow-loop_one.py
@@ -1,3 +1,14 @@
 #!/Users/tony/anaconda3/bin/python3
 
 print("=" * 20 + "Loop Statement with Break " + "=" * 20)
+
+
+name = ''
+
+while True:
+    print("Enter Your name : ")
+    name = input()
+    if name == 'Mark':
+        break
+
+print("Thank you")
