1ee8e2c42a539c403ca89d1f96caeb9f22b3f53c
Liu Zhenhua <join_github_repo@126.com>
2020-10-01 23:04:53 +0800

    Updating the Python File

--- Python-Learning-01/lang/flow/python_flow_three.py
+++ Python-Learning-01/lang/flow/python_flow_three.py
@@ -3,3 +3,14 @@
 """
 Python Flow
 """
+
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('Hello, '+name)
+elif age < 12:
+    print('You are not Alice, Kiddo')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
