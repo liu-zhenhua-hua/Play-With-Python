3b4d3700a038a62f87eefbb3fbf1288abadb37d8
Liu Zhenhua <join_github_repo@126.com>
2020-10-02 14:57:06 +0800

    Updating the Python File

--- Python-Learning-01/lang/flow/python_flow_loop_four.py
+++ Python-Learning-01/lang/flow/python_flow_loop_four.py
@@ -1,2 +1,15 @@
 #!/Users/tony/anaconda3/bin/python3
 
+"""
+Python for loop range
+"""
+
+for i in range(5,11):
+    print(i)
+
+print("=" * 50)
+
+for i in range(0,10,2):
+    print(i)
+
+print("=" * 50)
