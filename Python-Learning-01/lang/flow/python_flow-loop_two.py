2d11ce08fc36919d25dd83884e532f3dae003f1f
Liu Zhenhua <join_github_repo@126.com>
2020-10-02 13:34:39 +0800

    Updating the Python File

--- Python-Learning-01/lang/flow/python_flow-loop_two.py
+++ Python-Learning-01/lang/flow/python_flow-loop_two.py
@@ -11,6 +11,7 @@ while True:
         continue
     print('Hello, Joe, What is the password? (It is a fish.)')
     password = input()
+    print('Type your Password ')
     if password == 'swordfish':
         break;
 print("Access granted")
