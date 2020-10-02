58238a4d805a9907eb1267e507bb8620e6550955
Liu Zhenhua <join_github_repo@126.com>
2020-10-02 13:33:13 +0800

    Updating the Python File

--- Python-Learning-01/lang/flow/python_flow-loop_two.py
+++ Python-Learning-01/lang/flow/python_flow-loop_two.py
@@ -1,5 +1,17 @@
 #!/Users/tony/anaconda3/bin/python3
 
 """
-Python
+Python Continue or Break
 """
+
+while True:
+    print('Who are you ?')
+    name = input()
+    if name != 'Joe':
+        continue
+    print('Hello, Joe, What is the password? (It is a fish.)')
+    password = input()
+    if password == 'swordfish':
+        break;
+print("Access granted")
+
