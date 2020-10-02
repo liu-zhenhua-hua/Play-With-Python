3fe76aeebd56f2453df6f3a5fd2f22d2ccdc81a3
Liu Zhenhua <join_github_repo@126.com>
2020-10-02 14:47:38 +0800

    Updating Python File

--- Python-Learning-01/lang/flow/python_flow-loop_three.py
+++ Python-Learning-01/lang/flow/python_flow-loop_three.py
@@ -7,3 +7,16 @@ Python for loop
 print('My name is : ')
 for i in range(5):
     print('Jimmy Five Times (' + str(i) + ')')
+
+
+
+print("=" * 90)
+
+"""
+range() 函数,
+range() 可以传递三个参数, start,stop(not including),step
+"""
+
+for i in range(10,51):
+    print(i)
+
