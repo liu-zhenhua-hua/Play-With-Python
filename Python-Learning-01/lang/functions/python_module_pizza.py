f222cbd586056984756094a91baf036c5383bd67
Liu Zhenhua <join_github_repo@126.com>
2020-10-03 14:13:50 +0800

    Updating the Python File

--- Python-Learning-01/lang/functions/python_module_pizza.py
+++ Python-Learning-01/lang/functions/python_module_pizza.py
@@ -1,6 +1,5 @@
-#!/Users/tony/anaconda3/bin/python3
 
-def make_pizza(size,*toppings):
+def make_pizza(size ,*toppings):
     print('Making a ' + str(size) + '-inch pizza with the following toppings:')
     for topping in toppings:
         print('-' + topping)
