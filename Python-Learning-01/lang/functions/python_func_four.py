5cd9f4f6042dfa04738649478e4a6da56e66dc81
Liu Zhenhua <join_github_repo@126.com>
2020-10-04 10:57:52 +0800

    Updating the Python File

--- Python-Learning-01/lang/functions/python_func_four.py
+++ Python-Learning-01/lang/functions/python_func_four.py
@@ -9,7 +9,17 @@ def make_dinner(piece,*ingredient):
 
 def collect_person_info(first, last, *team_members, **member_info):
     """collect person information """
-
+    all_members = team_members
+    for member in all_members:
+        print(member)
+    profile = {}
+    profile['first'] = first
+    profile['last'] = last
+    for key, value in member_info.items():
+        profile[key] = value
+    print(profile)
 
 if __name__ == '__main__':
     make_dinner(2,'Fried','Peper','Apple','Vegetables')
+
+    collect_person_info('Mark','Zhang','James','William','Ray','Mercy',age=12,location='Dalian')
