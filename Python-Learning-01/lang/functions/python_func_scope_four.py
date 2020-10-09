40b28e3393fc455aedbc5b2dd654e8e18679f3a5
Liu Zhenhua <join_github_repo@126.com>
2020-10-09 14:54:47 +0800

    Updating the Python File

--- Python-Learning-01/lang/functions/python_func_scope_four.py
+++ Python-Learning-01/lang/functions/python_func_scope_four.py
@@ -1,8 +1,26 @@
 #!/Users/tony/anaconda3/bin/python3
 
 
+# def spam():
+#     print(eggs)
+# eggs = 10
+# spam()
+# print(eggs)
+
+"""
+Local and Global Variables with Same Name
+"""
+
 def spam():
+    eggs = 'spam local'
     print(eggs)
-eggs = 10
-spam()
+
+def bacon():
+    eggs = 'bacon local'
+    print(eggs)
+    spam()
+    print(eggs)
+
+eggs = 'global'
+bacon()
 print(eggs)
