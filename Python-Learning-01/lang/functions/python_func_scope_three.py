7af2f396c995ad186915a8f5c9f4ed74848099c9
Liu Zhenhua <join_github_repo@126.com>
2020-10-09 09:29:44 +0800

    Updating the Python File

--- Python-Learning-01/lang/functions/python_func_scope_three.py
+++ Python-Learning-01/lang/functions/python_func_scope_three.py
@@ -1,3 +1,14 @@
 #!/Users/tony/anaconda3/bin/python3
 
-import re
+g_number = 10 #global variable
+
+
+def function_scope(firstv, secondv):
+    print(g_number,firstv, secondv)
+
+
+
+
+
+if __name__ == '__main__':
+    function_scope(15, 20)
