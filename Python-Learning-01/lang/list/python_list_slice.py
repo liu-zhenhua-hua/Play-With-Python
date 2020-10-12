aa693722c8fbde373fcad1e9aeebed8bee420f43
Liu Zhenhua <join_github_repo@126.com>
2020-10-12 16:49:13 +0800

    Updating the Python File

---
+++ Python-Learning-01/lang/list/python_list_slice.py
@@ -0,0 +1,22 @@
+#!/Users/tony/anaconda3/bin/python3
+
+"""
+Python List中的slice
+spam[2]    : a List with index(one integer)
+spam[1:4]  : a list with a slice(two integer)
+spam[startIndex:endIndex(not include)], 不包含endIndex索引所对应的元素
+"""
+
+
+spam=['cat','bat','rat','elephant'] #定义一个list
+
+print(spam[1:4]) #['bat','rat','elephant']
+
+print(spam[0:4]) #print out the all elements of List
+
+
+print("*" * 100)
+
+spam=['cat','bat','rat','elephant']
+
+print(spam[:2])
