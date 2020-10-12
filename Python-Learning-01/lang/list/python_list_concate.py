e765ce2a75053ab8d7d24f4ddb5fb7f12b180b6e
Liu Zhenhua <join_github_repo@126.com>
2020-10-12 17:34:37 +0800

    Updating the Python File

---
+++ Python-Learning-01/lang/list/python_list_concate.py
@@ -0,0 +1,24 @@
+#!/Users/tony/anaconda3/bin/python3
+
+"""
+The + operator combines two lists to create a new list value
+and the * operator can be used with a list and an integer value
+to replicate the list
+"""
+
+"""
+使用 '+' 合并两个list
+"""
+alist = [1,3,5]
+blist = [2,4,6]
+
+print(alist + blist)
+
+#========================================
+
+print(['x','y','z'] * 4)
+
+#========================================
+spam = [12,56,90]
+spam = spam + ['B','D','Z']
+print(spam)
--- Python-Learning-01/lang/list/python_list_modify.py
+++ Python-Learning-01/lang/list/python_list_modify.py
@@ -10,7 +10,15 @@ spam=['cat','bat','rat','elephant']
 """
 通过索引修改list的值
 """
-
-spam[1] = ''
-
+spam[1] = 'Tiger'
 print(spam)
+
+#====================================================
+print('*' * 90)
+
+
+spam=['cat','bat','rat','elephant']
+spam[1] = 'Tiger'
+spam[2] = spam[1]
+print(spam)
+
