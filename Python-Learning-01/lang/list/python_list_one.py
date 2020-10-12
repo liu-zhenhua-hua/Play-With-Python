937dc85aaed21849e7634848bef065f75e3e8836
Liu Zhenhua <join_github_repo@126.com>
2020-10-12 13:50:20 +0800

    Updating the Python File

--- Python-Learning-01/lang/list/python_list_one.py
+++ Python-Learning-01/lang/list/python_list_one.py
@@ -5,10 +5,17 @@ Python List
 """
 
 spam=['cat','bat','rat','elephant']
-print(spam[0])
+print(spam[0]) #index 只能是整数, 不能是浮点数
 
+print(spam[int(2.1)]) #int() 转化成整数
 
 alist = ['cat','bat','rat','element']
 
-print(type(alist))
+# print(type(alist))
 
+
+"""
+包含list的list
+"""
+spam = [['cat','bat'],[10,20,30,40,50]]
+print(spam[0])
