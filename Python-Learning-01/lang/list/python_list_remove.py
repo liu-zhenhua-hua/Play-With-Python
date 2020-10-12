fcc3ef889ca9fb4c2dee12d1c2cb9ec2a8d72e1e
Liu Zhenhua <join_github_repo@126.com>
2020-10-12 17:38:56 +0800

    Updating the Python File

---
+++ Python-Learning-01/lang/list/python_list_remove.py
@@ -0,0 +1,15 @@
+#!/Users/tony/anaconda3/bin/python3
+
+"""
+removing the values from lists with del statement.
+
+del statement will delete values at an index in a list.
+"""
+
+"""
+del statement
+"""
+spam = ['cat','bat','rat','elephant']
+print(spam)
+del spam[1]
+print(spam)
