bd6152fe0e0879f1dfd472af23f695234a0cc330
Liu Zhenhua <join_github_repo@126.com>
2020-10-13 15:41:57 +0800

    Updating the Python File

---
+++ Python-Learning-01/lang/list/python_sequenceType.py
@@ -0,0 +1,20 @@
+#!/Users/tony/anaconda3/bin/python3
+
+"""
+The Python Sequence data Types
+list
+strings
+range objects return by range()
+tuples
+"""
+
+"""
+list    --> mutable data type (added, removed, changed)
+string  --> immutable data type
+"""
+
+name = 'Zophie a cat'
+newName = name[0:7] + 'the' + name[8:12]
+
+print(name)
+print(newName)
