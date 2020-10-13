803e6cdc47a807015ae7ed4dbf71b26c6e2d68fb
Liu Zhenhua <join_github_repo@126.com>
2020-10-13 13:38:42 +0800

    Updating the Python File

---
+++ Python-Learning-01/lang/list/python_list_enumerate.py
@@ -0,0 +1,8 @@
+#!/Users/tony/anaconda3/bin/python3
+
+"""
+range(len(list)) 除了使用这个方法遍历, 还可以使用enumerate()函数来实现
+enumerate() 返回两个值, list-index, list-value 索引和索引对应的值
+"""
+
+
--- Python-Learning-01/lang/list/python_list_multi_Assignment.py
+++ Python-Learning-01/lang/list/python_list_multi_Assignment.py
@@ -6,7 +6,7 @@ multiple assignment trick
 
 cat=['fat','gray','loud']
 
-size,color,disposition=cat
+size,color,disposition=cat #这样操作需要注意, 变量的个数与list的长度必须要一致, 否则会报错
 
 print(size)
 print(color)
