2caf821f8678b832322636a53934670db14ee7e2
Liu Zhenhua <join_github_repo@126.com>
2020-10-13 13:24:07 +0800

    Updating the Python File

---
+++ Python-Learning-01/lang/list/python_list_multi_Assignment.py
@@ -0,0 +1,13 @@
+#!/Users/tony/anaconda3/bin/python3
+
+"""
+multiple assignment trick
+"""
+
+cat=['fat','gray','loud']
+
+size,color,disposition=cat
+
+print(size)
+print(color)
+print(disposition)
