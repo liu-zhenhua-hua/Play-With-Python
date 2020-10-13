13ff0714c4f44defc30d46697fa6c1cfbaf8cc66
Liu Zhenhua <join_github_repo@126.com>
2020-10-13 13:07:22 +0800

    Updating the Python File

--- Python-Learning-01/lang/list/python_list_access.py
+++ Python-Learning-01/lang/list/python_list_access.py
@@ -3,3 +3,12 @@
 
 suppliers = ['Zeal','Zulu','Pens','Staples','flame','binders']
 
+for i in range(len(suppliers)):
+    print('Index ' + str(i) + 'in supplies is : ' + suppliers[i])
+
+
+
+#==================================================
+print('*' * 80)
+
+myPets=['Zophie','Pooka','Fat-tail']
