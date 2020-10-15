10cf24e3613798ba90eb0d0f2ebd93b7046518f6
Liu Zhenhua <join_github_repo@126.com>
2020-10-15 15:03:24 +0800

    Updating the Python File

--- Python-Learning-01/numpy/numpy_program_two.py
+++ Python-Learning-01/numpy/numpy_program_two.py
@@ -7,3 +7,17 @@ Python's range() function
 """
 alist = [i for i in range(0,20,2)]
 print(alist)
+
+"""
+numpy 中存在arange() 与python中的range()类似, 类似是因为step 步长可以是浮点数
+linspace(start,end) 这里返回的结果是包含start和end的值
+"""
+print(np.arange(10))
+print(np.arange(0,10,2))
+print(np.arange(0,5,0.4))
+
+print("=" * 90)
+
+print(np.linspace(0,20,11,dtype=int)) #步长是2, 0 ~ 20 需要11个数
+print(np.linspace(0,20,6,dtype=int))
+print(np.linspace(0,21,8,dtype=int))
