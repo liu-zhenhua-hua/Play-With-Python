6d7e7f15f8c4fb254480bebff4806fedb4c81dc4
Liu Zhenhua <join_github_repo@126.com>
2020-10-15 14:40:40 +0800

    Updating the Python File

--- Python-Learning-01/numpy/numpy_program_one.py
+++ Python-Learning-01/numpy/numpy_program_one.py
@@ -12,6 +12,20 @@ print("=" * 90)
 print(np.zeros((3,5)))
 
 
-zeros_array = np.zeros(shape=(3,5),dtype=int)
+zeros_array = np.zeros(shape=(3,5),dtype=int) #np.zeros 全0矩阵
 print(zeros_array)
 print(type(zeros_array))
+
+print("=" * 90)
+
+ones_array = np.ones(shape=(3,5),dtype=int) #np.ones 全1矩阵
+print(ones_array)
+
+
+print("=" * 90)
+
+full_array = np.full(shape=(3,5),fill_value=777)
+print(full_array)
+
+full_array_again = np.full((3,5),666)
+print(full_array_again)
