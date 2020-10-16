dbd7bb9b1b176727e3dfb8f15faba39e7ea2d4bf
Liu Zhenhua <join_github_repo@126.com>
2020-10-16 19:59:55 +0800

    Updating the Python File

--- Python-Learning-01/machinelearning/ml_dataload.py
+++ Python-Learning-01/machinelearning/ml_dataload.py
@@ -10,3 +10,9 @@ iris = datasets.load_iris() #sklearn自己定义的一个数据集, 它所返回
 print(iris.keys())
 
 print(iris.DESCR)
+
+print(iris.data) #查看测试数据, iris.data是numpy的一个对象
+
+# print(type(iris.data)) #<class 'numpy.ndarray'>
+
+print("The Shape of iris's Data {}".format(iris.data.shape))
