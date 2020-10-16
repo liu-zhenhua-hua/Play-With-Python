ae3a61ec5dd206549d427c7dac7a3eeae7d338ac
Liu Zhenhua <join_github_repo@126.com>
2020-10-16 19:44:41 +0800

    Updating the ml Python File

--- Python-Learning-01/lang/list/python_list_access.py
+++ Python-Learning-01/lang/list/python_list_access.py
@@ -18,3 +18,8 @@ if name not in myPets:
     print('I do not have a pet named ' + name)
 else:
     print(name + ' is my pet')
+
+#==================================================
+#=== List Reference ===
+
+
---
+++ Python-Learning-01/machinelearning/ml_dataload.py
@@ -0,0 +1,8 @@
+#!/Users/tony/anaconda3/bin/python3
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+
