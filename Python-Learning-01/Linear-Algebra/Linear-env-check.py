1b180a1f285e14c44689a0bfe7dfa3bedfd6753e
Liu Zhenhua <join_github_repo@126.com>
2020-10-20 10:20:57 +0800

    Updating the Python File

--- Python-Learning-01/Linear-Algebra/Linear-env-check.py
+++ Python-Learning-01/Linear-Algebra/Linear-env-check.py
@@ -1,8 +1,10 @@
 #!/Users/tony/anaconda3/bin/python3
 
+import sys
 import numpy
 import scipy
 
 if __name__ == '__main__':
+    print(sys.version)
     print(numpy.__version__)
     print(scipy.__version__)
