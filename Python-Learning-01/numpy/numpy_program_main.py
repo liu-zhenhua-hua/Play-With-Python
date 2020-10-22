0076f463119cbb6b8a29e23959cbf55a13748996
Liu Zhenhua <join_github_repo@126.com>
2020-10-22 16:17:00 +0800

    Updating the Python File

--- Python-Learning-01/numpy/numpy_program_main.py
+++ Python-Learning-01/numpy/numpy_program_main.py
@@ -10,3 +10,14 @@ if __name__ == '__main__':
     lst = [1,2,3]
     lst[0] = "Linear-Algebra"
     print(lst)
+
+    """
+        creating numpy vector object
+    """
+
+    vectorFirst = np.array([1,2,3]) #将Python的list作为参数传入array中
+    print(vectorFirst)
+
+    # vectorFirst[0]="Linear" #会报错, numpy中的ndarray对象,只能存储同一种数据类型
+
+    # print(type(vectorFirst))
