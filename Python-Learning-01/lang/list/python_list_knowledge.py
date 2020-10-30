7deb7a9bbf1378b1223f398f0be37a0557339b2f
Liu Zhenhua <join_github_repo@126.com>
2020-10-30 20:49:15 +0800

    Updating the Python File

--- Python-Learning-01/lang/list/python_list_knowledge.py
+++ Python-Learning-01/lang/list/python_list_knowledge.py
@@ -9,3 +9,25 @@ alist.append('d')
 
 
 print(alist)
+
+pointerA = alist
+pointerB = alist
+
+
+alist.append('zz')
+
+print(alist)
+
+print("The id of pointerA " + str(id(pointerA)))
+print("The id of pointerB " + str(id(pointerB)))
+
+print(pointerA)
+print(pointerB)
+
+#===========================================================================
+
+secondList = ['cat','bat','rat','elephant']
+
+print(secondList)
+secondList.clear()
+print(secondList)
