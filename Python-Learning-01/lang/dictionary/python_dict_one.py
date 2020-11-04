98a037e3abde48b29a22147e20940b7489218a10
Liu Zhenhua <join_github_repo@126.com>
2020-11-04 19:47:54 +0800

    Updating the Python File

--- Python-Learning-01/lang/dictionary/python_dict_one.py
+++ Python-Learning-01/lang/dictionary/python_dict_one.py
@@ -1,3 +1,11 @@
 #!/Users/tony/anaconda3/bin/python3
 
 
+
+myCat={'size: ':'fat',' color: ':'gray',' disposition: ': 'loud'}
+
+print(myCat)
+
+#access value through the key
+
+print('The Size of Cat : ' + myCat['size: '])
