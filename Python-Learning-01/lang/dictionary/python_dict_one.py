aaa53074cf2e8042947cfcc8e8cb3e99afba9445
Liu Zhenhua <join_github_repo@126.com>
2020-11-05 20:27:26 +0800

    Updating the Python File

--- Python-Learning-01/lang/dictionary/python_dict_one.py
+++ Python-Learning-01/lang/dictionary/python_dict_one.py
@@ -17,3 +17,17 @@ Dictionaries can still use integer values as keys
 spam={12:'Luggage Combination', 43: 'The Answer'}
 
 print('The Dictionary is : ' + spam[12])
+
+
+'''
+Unlike Lists, items in dictionaries are unordered. 
+There is no 'first' item in a dictionary. 
+'''
+
+aListA = ['cat','dog','rat']
+bListB = ['cat','rat','dog']
+
+if (aListA == bListB):
+    print('The Two List equals each other ')
+else:
+    print('The Two List are not equal ')
