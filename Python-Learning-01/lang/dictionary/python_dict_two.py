a4f5653be9eb489f010ef1df743e4a2458cf15e6
Liu Zhenhua <join_github_repo@126.com>
2020-11-06 19:33:53 +0800

    Updating the Python File

--- Python-Learning-01/lang/dictionary/python_dict_two.py
+++ Python-Learning-01/lang/dictionary/python_dict_two.py
@@ -5,3 +5,13 @@ eggs = {'name':'Zophie','species':'cat','age':'8'}
 
 list_eggs = list(eggs)
 print(list_eggs)
+
+'''
+keys(), values(), items()
+'''
+
+spam = {'color':'red','age':43,'name':'Tick'}
+
+for v in spam.values():
+    print(v)
+
