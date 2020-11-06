dd2e76aa17a4dbe1685dfa53dda3536fe8a93b34
Liu Zhenhua <join_github_repo@126.com>
2020-11-06 20:40:55 +0800

    Updating the Python File

--- Python-Learning-01/lang/dictionary/python_dict_three.py
+++ Python-Learning-01/lang/dictionary/python_dict_three.py
@@ -1,3 +1,19 @@
 #!/Users/tony/anaconda3/bin/python3
 
 
+spam = {'name':'Zophie','age':23}
+
+if 'name' in spam.keys():
+    print('True')
+
+if 'Zophie' in spam.values():
+    print('True')
+
+if 'color' in spam.keys():
+    print('False')
+
+
+if 'color' not in spam.keys():
+    print('True')
+
+print('color' in spam)
