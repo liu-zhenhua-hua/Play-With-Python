578daf76ab2f9dc94cd060e9fd95b18bad3b69a7
Liu Zhenhua <join_github_repo@126.com>
2020-11-09 20:09:08 +0800

    Updating the Python File

---
+++ Python-Learning-01/lang/string/python_string_one.py
@@ -0,0 +1,13 @@
+#!/Users/tony/anaconda3/bin/python3
+
+
+"""
+Python's String Escape Characters
+"""
+spam = 'Say hi to Bob\'s monther' #Escape Characters
+
+print(spam)
+
+secondSpam = 'Let\'s learning Python\tProgramming Language'
+
+print(secondSpam)
