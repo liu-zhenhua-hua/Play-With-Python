e34ca5df55b08bd8e8ac0784cfdd134b1b705d86
Liu Zhenhua <join_github_repo@126.com>
2020-11-10 20:32:52 +0800

    Updating the Python File

--- Python-Learning-01/lang/string/python_string_two.py
+++ Python-Learning-01/lang/string/python_string_two.py
@@ -7,3 +7,15 @@ age = 4000
 message='Hello, my name is ' + name + '. I am ' + str(age) + ' years old.'
 
 print(message)
+
+#==================================================================================
+
+secondMessage = 'My name is %s. I am %s years old. '%(name,age)
+
+print(secondMessage)
+
+#==================================================================================
+
+thirdMessage = f'My name is {name}. Next year I will be {age+1}.'
+
+print(thirdMessage)
