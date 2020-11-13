6bbde7392cbc0037b73a693dd874645a76c9e734
Liu Zhenhua <join_github_repo@126.com>
2020-11-13 21:07:08 +0800

    Updating the Python File

--- Python-Learning-01/lang/string/python_string_five.py
+++ Python-Learning-01/lang/string/python_string_five.py
@@ -1,3 +1,13 @@
 #!/Users/tony/anaconda3/bin/python3
 
 
+def formatStr(target,original):
+    if target == '' or target == 'python':
+        return
+    else:
+        target = 'This is a magic {}'.format(original)
+        return target
+
+
+if __name__ == '__main__':
+    print(formatStr(',','Characters'))
