b0bb030de2e5af6d1867564bcc8d8e1cb8f67c1d
Liu Zhenhua <join_github_repo@126.com>
2020-11-17 19:31:57 +0800

    Updating the Python File

--- Python-Learning-01/lang/string/python_string_six.py
+++ Python-Learning-01/lang/string/python_string_six.py
@@ -4,7 +4,17 @@
 def strJust(justlength):
     return 'Hello'.rjust(justlength,'=')
 
+def stripMethod(stripMessage):
+    spam = stripMessage
+    return spam.strip()
+
+
+def rightStripMethod(stripMessage):
+    spam = stripMessage
+
 
 
 if __name__ == '__main__':
     print(strJust(20))
+    #print('  Hello World  ')
+    #print(stripMethod('  Hello World  '))
