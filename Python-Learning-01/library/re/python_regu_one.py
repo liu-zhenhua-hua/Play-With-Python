138520a5267bd761854efca8656d12cf6616b996
Liu Zhenhua <join_github_repo@126.com>
2020-11-18 12:11:02 +0800

    Updating the Python File

--- Python-Learning-01/library/re/python_regu_one.py
+++ Python-Learning-01/library/re/python_regu_one.py
@@ -1,3 +1,6 @@
 #!/Users/tony/anaconda3/bin/python3
 
 import re
+
+phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') #now phoneRegex contains Regex Object <class '_sre.SRE_Pattern'>
+print(type(phoneRegex))
