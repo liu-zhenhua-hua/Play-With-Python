3a9ec764cd14cd7a5b3d7cb7a8f956f7f3a5724f
Liu Zhenhua <join_github_repo@126.com>
2020-11-18 16:56:15 +0800

    Updating the Python File

--- Python-Learning-01/library/re/python_regu_one.py
+++ Python-Learning-01/library/re/python_regu_one.py
@@ -1,9 +1,19 @@
 #!/Users/tony/anaconda3/bin/python3
 
 import re
+"""
+Regular Expression Matching
+1. Import the regex module with import re.
+2. Create a Regex object with the re.compile() function (Remember to use a raw string)
+3. Pass the String you want to search into the Regex Object's search() Method. This returns a Match Object
+4. Call the Match Object's group() method to return a string of the actual matched text.
+"""
 
 phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') #now phoneRegex contains Regex Object <class '_sre.SRE_Pattern'>
 
 myResult = phoneRegex.search('My Number is 415-555-4243')
 
 print('Phone Number is : ' + myResult.group())
+
+print('=' * 90)
+
---
+++ Python-Learning-01/library/re/python_regu_two.py
@@ -0,0 +1,3 @@
+#!/Users/tony/anaconda3/bin/python3
+
+import re
