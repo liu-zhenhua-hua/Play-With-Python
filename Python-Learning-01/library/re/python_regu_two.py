0a5b531a1f1d51b712fbf9fddfed4449f343d45b
Liu Zhenhua <join_github_repo@126.com>
2020-11-21 20:16:57 +0800

    Updating the Python File

--- Python-Learning-01/library/re/python_regu_two.py
+++ Python-Learning-01/library/re/python_regu_two.py
@@ -2,6 +2,11 @@
 
 import re
 
+"""
+using parentheses will create groups in the regex object.
+then use group() match object method to grab the matching text
+from just one group
+"""
 phoneNumberRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 
 #nameListRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
@@ -9,3 +14,4 @@ phoneNumberRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 phoneResult = phoneNumberRegex.search('My Phone Number is 415-555-4343')
 
 print(phoneResult.group(1))
+print(phoneResult.group(2))
