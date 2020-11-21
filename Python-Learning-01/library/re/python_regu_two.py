151f5d7f9bafcc0c311aada64c445524bc90d474
Liu Zhenhua <join_github_repo@126.com>
2020-11-21 22:06:34 +0800

    Updating the Python File

--- Python-Learning-01/library/re/python_regu_two.py
+++ Python-Learning-01/library/re/python_regu_two.py
@@ -13,6 +13,21 @@ phoneNumberRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 
 phoneResult = phoneNumberRegex.search('My Phone Number is 415-555-4343')
 
+
+"""
+The first set of parentheses in a regex string will be group 1. The second set will be group 2. 
+By passing the integer 1 or 2 to the group() match object method, you can grab different parts of the matched text. 
+Passing 0 or nothing to the group() method will return the entire matched text.
+
+if you would like to retrieve all the groups at once, use the groups() method note the plural
+"""
 print(phoneResult.group(1))
 print(phoneResult.group(2))
 print(phoneResult.group(0))
+
+
+mainNumber,secondNumber = phoneResult.groups() # multiple assignment tricks
+print(mainNumber)
+
+
+#===============================================================================================
