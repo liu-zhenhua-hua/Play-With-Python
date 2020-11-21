32b3fcf80c242aa7abdf3e4b24424e0aace3b187
Liu Zhenhua <join_github_repo@126.com>
2020-11-21 20:10:37 +0800

    Updating the Python File

--- Python-Learning-01/library/re/python_regu_two.py
+++ Python-Learning-01/library/re/python_regu_two.py
@@ -4,4 +4,8 @@ import re
 
 phoneNumberRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 
-nameListRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+#nameListRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+
+phoneResult = phoneNumberRegex.search('My Phone Number is 415-555-4343')
+
+print(phoneResult.group(1))
