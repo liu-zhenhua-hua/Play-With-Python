5e7b5482430704c411cdb167bc0edf10e2875755
Liu Zhenhua <join_github_repo@126.com>
2020-11-22 15:34:41 +0800

    Updating the Python File

--- Python-Learning-01/library/re/python_regu_three.py
+++ Python-Learning-01/library/re/python_regu_three.py
@@ -6,3 +6,18 @@ batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
 batRegexResult = batRegex.search('Batmobile lost a wheel')
 
 print(batRegexResult.group())
+print(batRegexResult.group(1))
+
+#===============================================================================================
+
+"""
+? -> precedes it as optional part of pattern
+* -> match zero or more
++ -> at least once
+"""
+
+patternRegexOne = re.compile(r'Bat(wo)?man')
+patternOneResult = patternRegexOne.search('This is our Batman')
+print(patternOneResult.group())
+patternOneResult = patternRegexOne.search('This is our Batwoman')
+print(patternOneResult.group())
