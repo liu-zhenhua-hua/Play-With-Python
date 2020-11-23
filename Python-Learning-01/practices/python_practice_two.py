65b149ca32b0b06b4504587a621660d2af5270d8
Liu Zhenhua <join_github_repo@126.com>
2020-11-23 19:48:53 +0800

    Updating the Python File

--- Python-Learning-01/practices/python_practice_two.py
+++ Python-Learning-01/practices/python_practice_two.py
@@ -13,3 +13,17 @@ Jack-k-k-k- Liu
 firstRegex = re.compile(r'(k-){4}') # (k-){4} will match k-k-k-k-, because, the group (k-) repeat 4 times, but k-k-k- will not.
 firstMatched = firstRegex.search(firstSearchText)
 print(firstMatched.group())
+
+"""
+Python’s regular expressions are greedy by default,
+"""
+secondSearchText="""HzHzHzHzHz"""
+secondRegex = re.compile(r'(Hz){3,5}') #greedy match, return 5 times of 'Hz'
+secondMatched = secondRegex.search(secondSearchText)
+print(secondMatched.group())
+
+
+thirdSearchText = """HzHzHzHzHzHzHzHzHz""" #non-greedy model, also called lazy mode
+thirdRegex = re.compile(r'(Hz){3,5}?') #non-gready model, also called Lazy mode
+thirdMatched = thirdRegex.search(thirdSearchText)
+print(thirdMatched.group())
