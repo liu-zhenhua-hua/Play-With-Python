ca41fa784fc536c1c0fb1c9da9a2831265406d4c
Liu Zhenhua <join_github_repo@126.com>
2020-11-23 20:44:30 +0800

    Updating the Python File

---
+++ Python-Learning-01/library/re/python_regu_five.py
@@ -0,0 +1,11 @@
+#!/Users/tony/anaconda3/bin/python3
+
+import re
+
+"""
+character classes
+"""
+
+firstSearchText = """
+
+"""
--- Python-Learning-01/library/re/python_regu_four.py
+++ Python-Learning-01/library/re/python_regu_four.py
@@ -3,3 +3,26 @@
 
 import re
 
+"""
+search() method will return a Match Object of the 'first' matched text in Searched String.
+findall() return a Match Object but a list of Strings 
+"""
+
+"""
+To summarize what the findall() method returns, remember the following:
+
+When called on a regex with no groups, such as \d\d\d-\d\d\d-\d\d\d\d, 
+the method findall() returns a list of string matches, such as ['415-555-9999', '212-555-0000'].
+
+
+When called on a regex that has groups, such as (\d\d\d)-(\d\d\d)-(\d\d\d\d), 
+the method findall() returns a list of tuples of strings (one string for each group), 
+such as [('415', '555', '9999'), ('212', '555', '0000')].
+
+"""
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') #has no group
+print(phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000'))
+
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)') #has no group
+print(phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000'))
