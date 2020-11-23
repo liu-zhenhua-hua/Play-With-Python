b21fe82f977ae94a722a9fadd4975a718cfa7bba
Liu Zhenhua <join_github_repo@126.com>
2020-11-23 10:28:45 +0800

    Updating the Python File

--- Python-Learning-01/practices/python_practice_one.py
+++ Python-Learning-01/practices/python_practice_one.py
@@ -8,9 +8,7 @@ define a text variable
 """
 search_text = """
 My Phone Numbers:
-345-789-3289
-897-900-5568
-808-111-9090
+345-789-3289 897-900-5568 808-111-9090
 """
 
 phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') #regex object return to phoneRegex
@@ -24,4 +22,16 @@ for items in phoneFetched:
     print(items.group())
 
 
-phoneNumberFetched = phoneRegex.search(search_text)
+# phoneNumberFetched = phoneRegex.search(search_text)
+# print(phoneNumberFetched.group())
+
+print("=" * 90)
+
+#===============================================================================================
+
+sec_search_text = """
+Phone Numbers List
+345-789-3289
+897-900-5568
+808-111-9090
+"""
