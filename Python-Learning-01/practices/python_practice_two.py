fe1cee1c5dc20b9a41b2d29c68fffa94e3e4e269
Liu Zhenhua <join_github_repo@126.com>
2020-11-23 19:28:08 +0800

    Updating the Python File

--- Python-Learning-01/practices/python_practice_two.py
+++ Python-Learning-01/practices/python_practice_two.py
@@ -10,6 +10,6 @@ firstSearchText = """
 Jack-k-k-k- Liu
 """
 
-firstRegex = re.compile(r'(k-){4}')
+firstRegex = re.compile(r'(k-){4}') # (k-){4} will match k-k-k-k-, because, the group (k-) repeat 4 times, but k-k-k- will not.
 firstMatched = firstRegex.search(firstSearchText)
 print(firstMatched.group())
