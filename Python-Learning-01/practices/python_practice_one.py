f2515695d2e1bf3bd3d359f7432ab3338c41311e
Liu Zhenhua <join_github_repo@126.com>
2020-11-23 09:35:33 +0800

    Updating the Python File

--- Python-Learning-01/practices/python_practice_one.py
+++ Python-Learning-01/practices/python_practice_one.py
@@ -3,3 +3,15 @@
 
 import re
 
+"""
+define a text variable
+"""
+search_text = """
+My Phone Numbers:
+345-789-3289
+897-900-5568
+808-111-9090
+"""
+
+phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') #regex object return to phoneRegex
+
