f9e0c2aa8798cbf1916330584918bcfd09901adb
Tony Liu <join_github_repo@126.com>
2023-10-25 09:22:10 +0800

    Updateing the Second Python code

--- Python-Learning-02/FirstPro.py
+++ Python-Learning-02/FirstPro.py
@@ -6,3 +6,17 @@ spam = 1 # and this is the second comment
          # ... and now a third
         
 text = "# This is not a comment because it's inside Quote"
+
+
+# Python - (Numbers)
+
+# The interpreter acts as a simple calculator:
+# >>> 2 + 2
+# 4
+# >>> 50 - 5*6
+# 20
+# >>> (50 - 5*6) / 4
+# 5.0
+# >>> 
+#
+#
